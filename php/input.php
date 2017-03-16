d7fabce0fc46f2652cae04403f3ac97f9a7edc46
Rodney Solomon <rodneysolomonjr@gmail.com>
2017-03-16 16:34:28 -0400

    initial commit
    push to github

---
+++ php/input.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JS &amp; PHP Project Project</title>
+	<link rel="stylesheet" type="text/css" href="../css/styles.css"> <!-- call to stylesheet --> 
+	
+</head>
+<body>
+	<?php
+		
+		function getAge($age){
+
+		echo "Consider the following: " . "<br>";	
+	    
+	    $message = null; # define the $message variable 
+	    
+	    if ($age <= 23)
+	    {
+	        $message = "You're still a pup! Stay in school.";
+	        
+	    }
+		
+		    elseif ($age > 24 && $age <= 38)
+		    {
+		       $message = "This is the prime of your life-- Enjoy it!"; 
+		    }
+		    elseif ($age > 38 && $age <= 49)
+		    {
+		        $message = "This is the time to focus on your career!";
+		    }
+		    elseif ($age > 49 && $age <= 59)
+		    {
+		        $message = "These might be your prime earning years.";
+		    }
+		    elseif ($age > 59 && $age <= 65)
+		    {
+		        $message = "Time to get ready for retirement.";
+		    }
+	    
+	    else
+	    {
+	        $message = "What's life's next adventure?";
+	    }
+
+	    	giveMessage($message); 
+		}
+		
+		getAge($_POST["age"]); # get from html form
+		
+		#DRY adding class to message
+		#After creating the function, I added extra styling to the message
+		
+		function giveMessage($message){
+			echo("<span class='message'>" . $message . "</span>"); 
+		}
+	?>
+</body>
+</html>
